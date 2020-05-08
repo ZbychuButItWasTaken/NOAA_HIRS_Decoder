5a754b8575d64feb9086c20ce527aa57861b48da
ZbychuButItWasTaken <sztanga.zbyszek@gmail.com>
2020-05-08 17:25:10 +0200

    Now using Maven to compile.

--- src/main/java/sh/surge/NOAA_HIRS_Decoder/Main.java
+++ src/main/java/sh/surge/NOAA_HIRS_Decoder/Main.java
@@ -15,8 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
+package sh.surge.NOAA_HIRS_Decoder;
 
-package com.zbychu;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
